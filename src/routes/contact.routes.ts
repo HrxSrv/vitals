1db1399158faef7f2b34378a840c45b933e28ef3
HrxSrv <harshitsrv2004@gmail.com>
2026-03-31 14:56:30 +0530

    bump: supabase update

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import { Router, type Router as RouterType } from 'express';
 import { postContactController } from '../controllers/contact/post.controller';
 
-const router = Router();
+const router: RouterType = Router();
 
 router.post('/', postContactController);
 
